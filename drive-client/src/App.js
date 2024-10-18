00cad5fb4bac059aa9208f9d1b251cc4a64a16f5
ashinjoy <ashinjoy22@gmail.com>
2024-10-18 22:19:48 +0530

    checking bugs

--- drive-client/src/App.js
+++ drive-client/src/App.js
@@ -3,7 +3,7 @@ import Toast from "./Utils/CustomToasts/Toast";
 import UserSearchContext from "./Context/UserSearchContext";
 import DriverLocation from "./Context/DriverLocation";
 import ChatProvider from "./Context/ChatProvider";
-console.log('helllo');
+console.log('hesslllo');
 
 function App() {
   return (
