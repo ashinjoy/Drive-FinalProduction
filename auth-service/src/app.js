daa0dddbd1f335218ab92c07525282dc0387bc18
ashinjoy <ashinjoy22@gmail.com>
2024-10-18 18:57:24 +0530

    checking the pipeline

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -1,6 +1,8 @@
 import createServer from "./framework/express.js";
 import { dbConnect } from "./config/dbConnect.js";
 import { configDotenv } from "dotenv";
+console.log('hello');
+
 
 configDotenv();
 
