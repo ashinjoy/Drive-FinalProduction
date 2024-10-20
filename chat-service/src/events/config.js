8eb8b318add7d51548cc585cd3b1a7a2dfc14d3e
ashinjoy <ashinjoy22@gmail.com>
2024-10-20 17:49:52 +0530

    changes in chat-srv

--- chat-service/src/events/config.js
+++ chat-service/src/events/config.js
@@ -3,4 +3,4 @@ export const TOPICS = ['AUTH_TOPIC','TRIP_TOPIC']
 export const USER_CREATED = 'USER_CREATED'
 export const USER_UPDATED = 'USER_UPDATED'
 export const TRIP_CREATED = 'TRIP_CREATED'
-export const TRIP_UPDATED = 'TRIP_UPDATED'
+export const TRIP_UPDATED = 'TRIP_UPDATED'  
