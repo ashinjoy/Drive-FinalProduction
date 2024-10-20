ecbe6ffedb4d02cee3033833c56cdf3a0250c704
ashinjoy <ashinjoy22@gmail.com>
2024-10-20 17:46:31 +0530

    change in auth-srv

--- auth-service/src/events/config.js
+++ auth-service/src/events/config.js
@@ -3,3 +3,4 @@ export const USER_CREATED = "USER_CREATED";
 export const USER_UPDATED = "USER_UPDATED";
 export const DRIVER_CREATED = "DRIVER_CREATED";
 export const DRIVER_UPDATED = "DRIVER_UPDATED";
+   
